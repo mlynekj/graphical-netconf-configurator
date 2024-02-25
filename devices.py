1882afe55965c58042e025be5fadd38f101718f5
yakubcze <jakub.mlynek32@gmail.com>
2024-02-25 20:58:04 +0100

    dynamicke propojeni zarizeni i pri pohybu zarizenim po platne, ohraniceni kolem routeru pro debug

--- devices.py
+++ devices.py
@@ -1,11 +1,53 @@
-from PySide6.QtWidgets import QGraphicsPixmapItem, QGraphicsItem
-from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtWidgets import QGraphicsPixmapItem, QGraphicsItem, QGraphicsLineItem, QGraphicsRectItem, QGraphicsSceneMouseEvent
+from PySide6.QtGui import QImage, QPixmap, QPen, QColor
+from PySide6.QtCore import QLineF
 
 class Router(QGraphicsPixmapItem):
-    def __init__(self):
+    def __init__(self, x=0, y=0):
         super().__init__()
 
         router_image_file = QImage("router.png")
 
         self.setPixmap(QPixmap.fromImage(router_image_file))
         self.setFlag(QGraphicsItem.ItemIsMovable, True)
+        self.setTransformOriginPoint(self.boundingRect().center())
+        self.setPos(x, y)
+        
+        self.cables = []
+
+        #DEBUG: zobrazi ohraniceni kolem routeru
+        if __debug__:
+            self.border = QGraphicsRectItem(self.boundingRect())
+        
+    def mouseMoveEvent(self, event):
+        super().mouseMoveEvent(event)
+        
+        for cable in self.cables:
+            cable.updatePosition()
+        
+        #DEBUG: zobrazi ohraniceni kolem routeru
+        if __debug__:
+            self.updateBorderPosition()
+
+    def updateBorderPosition(self):
+        self.border.setPos(self.scenePos())
+
+class Cable(QGraphicsLineItem):
+    def __init__(self, device_1, device_2):
+        super().__init__()
+
+        self.device_1 = device_1
+        self.device_2 = device_2
+
+        self.device_1.cables.append(self)
+        self.device_2.cables.append(self)
+
+        self.setPen(QPen(QColor(0, 0, 0), 3))
+
+        self.updatePosition()
+
+    def updatePosition(self):
+        self.device_1_center = self.device_1.sceneBoundingRect().center()
+        self.device_2_center = self.device_2.sceneBoundingRect().center()
+
+        self.setLine(self.device_1_center.x(), self.device_1_center.y(), self.device_2_center.x(), self.device_2_center.y())
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene
-from devices import Router
+from PySide6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsLineItem, QGraphicsItem, QGraphicsRectItem
+from PySide6.QtGui import QPen
+from devices import Router, Cable
 
 class MainView(QGraphicsView):
     def __init__(self):
@@ -14,15 +15,33 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("Network Configurator")
         self.view = MainView()
         self.setCentralWidget(self.view)
 
-        self.router = Router()
-        self.view.scene.addItem(self.router)
+        router = self.addRouter(100, 100)
+        router2 = self.addRouter(300, 300)
 
-        self.router2 = Router()
-        self.view.scene.addItem(self.router2)
+        cable = self.addCable(router, router2)
+        
+    def addRouter(self, x, y):
+        router = Router(x, y)
+        self.view.scene.addItem(router)
+        
+        #DEBUG: zobrazi ohraniceni kolem routeru
+        if __debug__:
+            self.view.scene.addItem(router.border)
+            router.border.setPos(router.scenePos())
+            
+        return router
+    
+    def addCable(self, device_1, device_2):
+        cable = Cable(device_1, device_2)
+        cable.setZValue(-1)                     #presunout objekt "cable" pod vsechny ostatni objekty
+        self.view.scene.addItem(cable)
+
+        return cable
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
