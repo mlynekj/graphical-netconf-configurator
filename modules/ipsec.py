f2160a9433034a6e2d0434b6add287ecb4e922d5
Jakub Mlýnek <jakub.mlynek32@gmail.com>
2025-03-05 20:12:51 +0100

    tweaks to ipsec dialog - "Interfaces" tab ui elements completed

--- modules/ipsec.py
+++ modules/ipsec.py
@@ -53,18 +53,18 @@ class IPSECDialog(QDialog):
         self.ui.setupUi(self)
 
         self.ipsec_scene = QGraphicsScene()
-        scene_height = self.ui.graphicsView.height()
-        device_width = cloned_devices[0].boundingRect().width()
-        spacing = 200
-        vertical_position = 50
         self.dev1 = cloned_devices[0]
         self.dev2 = cloned_devices[1]
 
-        ipsec_scheme = QPixmap("graphics/ipsec_scheme.png")
-        self.ipsec_scene.addPixmap(ipsec_scheme)
+        self.ipsec_scheme_background = QPixmap("graphics/ipsec_scheme.png")
+        self.ipsec_scene.addPixmap(self.ipsec_scheme_background)
 
-        # AZ BUDE FUNKCNI NETCONF SPOJENI, CELE TOTO PREPSAT DO NECEHO CITELNEJSIHO
-        # device1 hostname label
+        self._fillIPSECScene()
+        self.ui.graphicsView.setScene(self.ipsec_scene)
+
+    def _fillIPSECScene(self):
+        # DEVICE1
+        # Hostname
         self.dev1_label_holder = QGraphicsRectItem()
         self.dev1_label = QGraphicsTextItem()
         self.dev1_label.setFont(QFont('Arial', 10))
@@ -75,8 +75,38 @@ class IPSECDialog(QDialog):
         self.dev1_label_holder.setPos(75, 90)
         self.dev1_label.setParentItem(self.dev1_label_holder)
         self.ipsec_scene.addItem(self.dev1_label_holder)
+        # Interfaces tab - groupbox
+        self.ui.dev1_groupbox.setTitle(self.dev1.original_device.hostname)
+        self.ui.dev1_LAN_interface_combobox.addItems(self.dev1.original_device.interfaces.keys())
+        self.ui.dev1_WAN_interface_combobox.addItems(self.dev1.original_device.interfaces.keys())
+        self.ui.dev1_LAN_interface_combobox.setCurrentIndex(-1)
+        self.ui.dev1_WAN_interface_combobox.setCurrentIndex(-1)
+        self.ui.dev1_LAN_interface_combobox.currentTextChanged.connect(lambda selected_intf = self.ui.dev1_LAN_interface_combobox.currentText: self._dev1_LAN_interface_selected(selected_intf))
+        self.ui.dev1_WAN_interface_combobox.currentTextChanged.connect(lambda selected_intf = self.ui.dev1_LAN_interface_combobox.currentText: self._dev1_WAN_interface_selected(selected_intf))
+        # Labels in the IPSECScene - LAN Network
+        self.ui.dev1_LAN_network_lineedit.setReadOnly(True)
+        self.dev1_lan_network_label = QGraphicsTextItem()
+        self.dev1_lan_network_label.setFont(QFont("Arial", 10))
+        self.dev1_lan_network_label.setPos(125, 250)
+        self.ipsec_scene.addItem(self.dev1_lan_network_label)
+        # Labels in the IPSECScene - LAN Interface
+        self.dev1_lan_interface_label = QGraphicsTextItem()
+        self.dev1_lan_interface_label.setFont(QFont("Arial", 10))
+        self.ipsec_scene.addItem(self.dev1_lan_interface_label)
+        # Labels in the IPSECScene - WAN Interface
+        self.dev1_wan_interface_label = QGraphicsTextItem()
+        self.dev1_wan_interface_label.setFont(QFont("Arial", 10))
+        self.dev1_wan_interface_label.setPos(130, 80)
+        self.ipsec_scene.addItem(self.dev1_wan_interface_label)
+        # Labels in the IPSECScene - WAN IP Address
+        self.ui.dev1_WAN_ip_lineedit.setReadOnly(True)
+        self.dev1_wan_ip_label = QGraphicsTextItem()
+        self.dev1_wan_ip_label.setFont(QFont("Arial", 10))
+        self.dev1_wan_ip_label.setPos(130, 100)
+        self.ipsec_scene.addItem(self.dev1_wan_ip_label)
 
-        # device2 hostname label
+        # DEVICE2
+        # Hostname
         self.dev2_label_holder = QGraphicsRectItem()
         self.dev2_label = QGraphicsTextItem()
         self.dev2_label.setFont(QFont('Arial', 10))
@@ -87,66 +117,38 @@ class IPSECDialog(QDialog):
         self.dev2_label_holder.setPos(505, 90)
         self.dev2_label.setParentItem(self.dev2_label_holder)
         self.ipsec_scene.addItem(self.dev2_label_holder)
-
-        self.ui.graphicsView.setScene(self.ipsec_scene)
-
-        # device1 interface tab
-        self.ui.dev1_groupbox.setTitle(self.dev1.original_device.hostname)
-        self.ui.dev1_LAN_interface_combobox.addItems(self.dev1.original_device.interfaces.keys())
-        self.ui.dev1_WAN_interface_combobox.addItems(self.dev1.original_device.interfaces.keys())
-        self.ui.dev1_LAN_interface_combobox.currentTextChanged.connect(lambda selected_intf = self.ui.dev1_LAN_interface_combobox.currentText: self._dev1_LAN_interface_selected(selected_intf))
-        self.ui.dev1_WAN_interface_combobox.currentTextChanged.connect(lambda selected_intf = self.ui.dev1_LAN_interface_combobox.currentText: self._dev1_WAN_interface_selected(selected_intf))
-
-        self.dev1_lan_network_label = QGraphicsTextItem()
-        self.dev1_lan_network_label.setFont(QFont("Arial", 10))
-        self.dev1_lan_network_label.setPos(125, 250) #toto asi neni stred, ale levy kraj? vylepsit
-        self.ipsec_scene.addItem(self.dev1_lan_network_label)
-
-        self.dev1_lan_interface_label = QGraphicsTextItem()
-        self.dev1_lan_interface_label.setFont(QFont("Arial", 10))
-        self.dev1_lan_interface_label.setPos(80, 120) #toto asi neni stred, ale levy kraj? vylepsit
-        self.ipsec_scene.addItem(self.dev1_lan_interface_label)
-
-        self.dev1_wan_interface_label = QGraphicsTextItem()
-        self.dev1_wan_interface_label.setFont(QFont("Arial", 10))
-        self.dev1_wan_interface_label.setPos(130, 85) #toto asi neni stred, ale levy kraj? vylepsit
-        self.ipsec_scene.addItem(self.dev1_wan_interface_label)
-
-        self.dev1_wan_ip_label = QGraphicsTextItem()
-        self.dev1_wan_ip_label.setFont(QFont("Arial", 10))
-        self.dev1_wan_ip_label.setPos(130, 105) #toto asi neni stred, ale levy kraj? vylepsit
-        self.ipsec_scene.addItem(self.dev1_wan_ip_label)
-
-        # device2 interface tab
+        # Interfaces tab - groupbox
         self.ui.dev2_groupbox.setTitle(self.dev2.original_device.hostname)
         self.ui.dev2_LAN_interface_combobox.addItems(self.dev2.original_device.interfaces.keys())
         self.ui.dev2_WAN_interface_combobox.addItems(self.dev2.original_device.interfaces.keys())
-        self.ui.dev2_LAN_interface_combobox.currentTextChanged.connect(lambda selected_intf = self.ui.dev1_LAN_interface_combobox.currentText: self._dev2_LAN_interface_selected(selected_intf))
-        self.ui.dev2_WAN_interface_combobox.currentTextChanged.connect(lambda selected_intf = self.ui.dev1_LAN_interface_combobox.currentText: self._dev2_WAN_interface_selected(selected_intf))
-
+        self.ui.dev2_LAN_interface_combobox.setCurrentIndex(-1)
+        self.ui.dev2_WAN_interface_combobox.setCurrentIndex(-1)
+        self.ui.dev2_LAN_interface_combobox.currentTextChanged.connect(lambda selected_intf = self.ui.dev2_LAN_interface_combobox.currentText: self._dev2_LAN_interface_selected(selected_intf))
+        self.ui.dev2_WAN_interface_combobox.currentTextChanged.connect(lambda selected_intf = self.ui.dev2_LAN_interface_combobox.currentText: self._dev2_WAN_interface_selected(selected_intf))
+        # Labels in the IPSECScene - LAN Network
+        self.ui.dev2_LAN_network_lineedit.setReadOnly(True)
         self.dev2_lan_network_label = QGraphicsTextItem()
         self.dev2_lan_network_label.setFont(QFont("Arial", 10))
-        self.dev2_lan_network_label.setPos(385, 250) #toto asi neni stred, ale levy kraj? vylepsit
+        self.dev2_lan_network_label.setPos(385, 250)
         self.ipsec_scene.addItem(self.dev2_lan_network_label)
-
+        # Labels in the IPSECScene - LAN Interface
         self.dev2_lan_interface_label = QGraphicsTextItem()
         self.dev2_lan_interface_label.setFont(QFont("Arial", 10))
-        self.dev2_lan_interface_label.setPos(510, 120) #toto asi neni stred, ale levy kraj? vylepsit
         self.ipsec_scene.addItem(self.dev2_lan_interface_label)
-
+        # Labels in the IPSECScene - WAN Interface
         self.dev2_wan_interface_label = QGraphicsTextItem()
         self.dev2_wan_interface_label.setFont(QFont("Arial", 10))
-        self.dev2_wan_interface_label.setPos(460, 85) #toto asi neni stred, ale levy kraj? vylepsit
         self.ipsec_scene.addItem(self.dev2_wan_interface_label)
-
+        # Labels in the IPSECScene - WAN IP Address
+        self.ui.dev2_WAN_ip_lineedit.setReadOnly(True)
         self.dev2_wan_ip_label = QGraphicsTextItem()
         self.dev2_wan_ip_label.setFont(QFont("Arial", 10))
-        self.dev2_wan_ip_label.setPos(460, 105) #toto asi neni stred, ale levy kraj? vylepsit
         self.ipsec_scene.addItem(self.dev2_wan_ip_label)
 
     def _dev1_LAN_interface_selected(self, selected_intf):
         interface_data = self.dev1.original_device.interfaces[selected_intf]
         self.dev1_lan_interface_label.setPlainText(selected_intf)
+        self._alignToCenter(self.dev1_lan_interface_label, QPointF(80, 120))
         ipv4_data, ipv6_data = utils.getFirstIPAddressesFromSubinterfaces(interface_data['subinterfaces'])
         if ipv4_data:
             self.ui.dev1_LAN_network_lineedit.setText(str(ipv4_data["value"].network))
@@ -159,8 +161,9 @@ class IPSECDialog(QDialog):
             self.dev1_lan_network_label.setPlainText("")
 
     def _dev2_LAN_interface_selected(self, selected_intf):
-        interface_data = self.dev1.original_device.interfaces[selected_intf]
+        interface_data = self.dev2.original_device.interfaces[selected_intf]
         self.dev2_lan_interface_label.setPlainText(selected_intf)
+        self._alignToCenter(self.dev2_lan_interface_label, QPointF(500, 120))
         ipv4_data, ipv6_data = utils.getFirstIPAddressesFromSubinterfaces(interface_data['subinterfaces'])
         if ipv4_data:
             self.ui.dev2_LAN_network_lineedit.setText(str(ipv4_data["value"].network))
@@ -188,13 +191,24 @@ class IPSECDialog(QDialog):
 
     def _dev2_WAN_interface_selected(self, selected_intf):
         interface_data = self.dev2.original_device.interfaces[selected_intf]
+        self.dev2_wan_interface_label.setPlainText(selected_intf)
+        self._alignToRight(self.dev2_wan_interface_label, QPointF(470, 80))
         ipv4_data, ipv6_data = utils.getFirstIPAddressesFromSubinterfaces(interface_data['subinterfaces'])
         if ipv4_data:
             self.ui.dev2_WAN_ip_lineedit.setText(str(ipv4_data["value"]))
             self.dev2_wan_ip_label.setPlainText(str(ipv4_data["value"]))
+            self._alignToRight(self.dev2_wan_ip_label, QPointF(470, 100))
         elif ipv6_data:
             self.ui.dev2_WAN_ip_lineedit.setText(str(ipv6_data["value"]))
             self.dev2_wan_ip_label.setPlainText(str(ipv6_data["value"]))
         else:
             self.ui.dev2_WAN_ip_lineedit.clear()
             self.ui.dev2_WAN_ip_lineedit.setText("")
+
+    def _alignToCenter(self, element_to_center, starting_pos):
+        elements_bounding_rect = element_to_center.boundingRect().width()
+        element_to_center.setPos(starting_pos.x() - elements_bounding_rect / 2, starting_pos.y())
+
+    def _alignToRight(self, element_to_align, starting_pos):
+        elements_bounding_rect = element_to_align.boundingRect().width()
+        element_to_align.setPos(starting_pos.x() - elements_bounding_rect, starting_pos.y())
